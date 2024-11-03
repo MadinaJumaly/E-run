323e8832d217b9ac488c60228fefd46c634fe2a4
MadinaJumaly <madinajumaly@gmail.com>
2024-11-03 18:28:33 +0500

    Initial Commit

---
+++ E-run/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  E-run
+//
+//  Created by Madina Jumaly on 03.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ E-run/E_runApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_runApp.swift
+//  E-run
+//
+//  Created by Madina Jumaly on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct E_runApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
